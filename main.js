6fff10e3df5fcf4e4e100b4460c6cde1a41c6867
Minalinsky <alyson1907@gmail.com>
2019-04-15 21:15:05 -0300

    3rd commit - MDN Solved

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var customName = document.getElementById('nome');
 var randomize = document.querySelector('.randomize');
 var story = document.querySelector('.story');
 
-function randomValueFromArray(array){
-  var random = Math.floor(Math.random()*array.length);
-  return array[random];
+function getRandomValue(textArray){
+  var random = Math.floor(Math.random()*textArray.length);
+  return textArray[random];
 }
 
 var completeText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
@@ -17,9 +17,9 @@ randomize.addEventListener('click', result);
 
 function result() {
   var generatedStory = completeText; // generatedStory é o newStory do enunciado
-  var x = randomValueFromArray(insertX); //xItem dado no enunciado
-  var y = randomValueFromArray(insertY); //yItem dado no enunciado
-  var z = randomValueFromArray(insertZ); //zItem dado no enunciado
+  var x = getRandomValue(insertX); //xItem dado no enunciado
+  var y = getRandomValue(insertY); //yItem dado no enunciado
+  var z = getRandomValue(insertZ); //zItem dado no enunciado
 
   generatedStory = generatedStory.replace(':insertx:',x);
   generatedStory = generatedStory.replace(':insertx:',x);
@@ -34,7 +34,7 @@ function result() {
   if(document.getElementById("uk").checked) {
     var weight = Math.round(300*0.0714286) + ' stone';
     var temperature =  Math.round((94-32) * 5 / 9) + ' centigrade';
-    generatedStory = generatedStory.replace('94 fahrenheit',temperature);
+    generatedStory = generatedStory.replace('94 fahrenheit', temperature);
     generatedStory = generatedStory.replace('300 pounds',weight);
   }
 
